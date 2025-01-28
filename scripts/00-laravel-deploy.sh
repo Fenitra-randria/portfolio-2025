27f012112fd824e193d64ffdbb772405b5fece09
Fenitra Randriamihajanirina <fenitra.randriamihajanirina@pulse.mg>
2025-01-28 15:52:56 +0300

    dsfs

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -8,11 +8,3 @@ echo "Caching routes..."
 php artisan route:cache
 echo "Running migrations..."
 php artisan migrate --force
-
-# Importer le fichier SQL dans la base de données
-echo "Importing database from backup.sql..."
-if [ -f /backup.sql ]; then
-    mysql -u root -p${DB_PASSWORD} ${DB_DATABASE} < /backup.sql
-    echo "Database import completed successfully."
-else
-    echo "Error: /backup.sql not found!"
